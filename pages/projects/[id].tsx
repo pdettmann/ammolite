d51cc4f5ede3aa496ae39f1ac240c9d6d92175fc
Paula Dettmann <paula.dettmann@gmail.com>
2022-06-12 22:20:59 +0200

    add homepage with login

---
+++ pages/projects/[id].tsx
@@ -0,0 +1,22 @@
+// function Project({ data }) {
+//     // Render data...
+//     return (
+//         <h1>Project</h1>
+//     )
+// }
+
+// export async function getServerSidePaths() {
+// // Return a list of possible value for id
+// }
+
+// // This gets called on every request
+// export async function getServerSideProps(params) {
+//     // Fetch data from external API
+//     const res = await fetch(``)
+//     const data = await res.json()
+
+//     // Pass data to the page via props
+//     return { props: { data } }
+// }
+
+// export default Project
