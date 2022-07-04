0e23d6a17f116f6558bd2bd72cabf800efa998f9
Paula Dettmann <paula.dettmann@gmail.com>
2022-07-04 10:38:38 +0200

    change jest config

--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,7 @@ export default createJestConfig({
   moduleNameMapper: {
     '^@/components/(.*)$': '<rootDir>/components/$1',
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    "^~(.*)$": "<rootDir>/src/$1"
   },
   testEnvironment: 'jest-environment-jsdom',
   // transform: { "^.+\\.(ts|tsx|js|jsx)?$": "ts-jest" }
