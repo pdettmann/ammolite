3f638203f257d1204fddef24d1e83dcd755fd036
Paula Dettmann <paula.dettmann@gmail.com>
2022-07-04 16:20:54 +0200

    add create project title css

--- pages/projects/create.tsx
+++ pages/projects/create.tsx
@@ -1,6 +1,6 @@
 import type { NextPage, NextPageContext } from 'next'
 import Layout from '../../components/layout'
-import { FormEvent, useState } from 'react'
+import { useState } from 'react'
 import { createProjectSubmit } from '../../lib/apiUtils'
 import { NextRouter, useRouter } from 'next/router'
 import ErrorBoundary from '../../lib/errorBoundary'
